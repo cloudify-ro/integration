ac483198a96266f2b87746f40b30d06a1a76cf85
DragomirAlin <dragomirdanielalin@yahoo.com>
2021-07-20 16:28:58 +0300

    enable features to objectMapper

--- java-integration/src/main/java/ro/cloudify/Main.java
+++ java-integration/src/main/java/ro/cloudify/Main.java
@@ -1,12 +1,15 @@
 package ro.cloudify;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.openstack4j.api.Builders;
 import org.openstack4j.api.OSClient;
 import org.openstack4j.api.types.Facing;
@@ -15,6 +18,7 @@ import org.openstack4j.model.compute.Server;
 import org.openstack4j.model.compute.ServerCreate;
 import org.openstack4j.model.identity.v3.Token;
 import org.openstack4j.openstack.OSFactory;
+import org.openstack4j.openstack.identity.v3.domain.KeystoneToken;
 import org.openstack4j.openstack.internal.OSClientSession;
 import ro.cloudify.model.ApplicationCredentials;
 import ro.cloudify.model.ApplicationCredentialsRequest;
@@ -28,7 +32,6 @@ import java.util.Map;
 @Slf4j
 public class Main {
     public static String endpoint = "https://id.cloud.acvile.com/v3";
-    public static String region = "eu-east-1";
     public static final String ID = "";
     public static final String SECRET = "";
 
@@ -37,7 +40,7 @@ public class Main {
         OSClient.OSClientV3 client = getClient();
 
         //List images
-        client.imagesV2().list();//
+        client.imagesV2().list();
 
         // List flavors
         client.compute().flavors().list();
@@ -57,14 +60,7 @@ public class Main {
     }
 
     private static OSClientSession.OSClientSessionV3 getClient() {
-        String plainToken = getToken();
-
-        Token token = OSFactory.builderV3()
-                .endpoint(endpoint)
-                .token(plainToken)
-                .authenticate()
-                .useRegion(region)
-                .getToken();
+        Token token = getToken();
 
         OSClientSession.OSClientSessionV3 sessionV3 = (OSClientSession.OSClientSessionV3) OSFactory.clientFromToken(token, Facing.PUBLIC);
 
@@ -74,10 +70,9 @@ public class Main {
         return sessionV3;
     }
 
-    private static String getToken() {
+    private static Token getToken() {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         ObjectMapper objectMapper = new ObjectMapper();
-
         ApplicationCredentialsRequest applicationCredentials = ApplicationCredentialsRequest.builder()
                 .auth(Auth.builder()
                         .identity(Identity.builder()
@@ -90,7 +85,7 @@ public class Main {
                         .build())
                 .build();
 
-        String token = null;
+        Token token = null;
         try {
             StringEntity postingString = new StringEntity(objectMapper.writeValueAsString(applicationCredentials));
             HttpPost post = new HttpPost(String.format("%s/auth/tokens", endpoint));
@@ -98,7 +93,11 @@ public class Main {
             post.setHeader("Content-type", "application/json");
 
             CloseableHttpResponse response = httpClient.execute(post);
-            token = response.getFirstHeader("x-subject-token").getValue();
+            String tokenResponse = EntityUtils.toString(response.getEntity());
+
+            objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+            objectMapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+            token = objectMapper.readValue(tokenResponse, KeystoneToken.class);
         } catch (Exception e) {
             log.error("An error occurred while getting the token.", e);
         }
